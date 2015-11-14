aa4a3012539c5303828d1c6d122ec590955463d6
Roman Olesyuk <roman.olesyuk@gmail.com>
2015-11-14 19:41:50 +0300

    Modified build script to be kernel version independent (almost). Takes versions from build directory.

--- build-kernel.sh
+++ build-kernel.sh
@@ -2,9 +2,13 @@
 
 #DEPENDENCIES="kernel-package libncurses5 libncurses5-dev build-essential patch fakeroot bc"
 
-KERNEL_MAJOR="4.1"
-KERNEL_MINOR="13"
-BFS="ck2"
+KERNEL_VERSION=`basename $(pwd) | awk -F '-' '{print $2}'`
+KERNEL_MAJOR=`echo ${KERNEL_VERSION} | awk -F '.' '{print $1 "." $2}'`
+KERNEL_MINOR=`echo ${KERNEL_VERSION} | awk -F '.' '{print $3}'`
+if [ -n "${KERNEL_MINOR}" ]; then
+    KERNEL_MINOR=".${KERNEL_MINOR}"
+fi
+BFS=`basename $(pwd) | awk -F '-' '{print $3}'`
 BFQ="v7r8"
 PREEMPT_RT=
 
@@ -18,15 +22,18 @@ big_echo() {
 	echo
 }
 
-big_echo "Downloading vanilla kernel ${KERNEL_MAJOR}.${KERNEL_MINOR} ..."
-wget -nc "https://www.kernel.org/pub/linux/kernel/v4.x/linux-${KERNEL_MAJOR}.${KERNEL_MINOR}.tar.xz" || exit 1
-wget -nc "https://www.kernel.org/pub/linux/kernel/v4.x/linux-${KERNEL_MAJOR}.${KERNEL_MINOR}.tar.sign" || exit 1
+big_echo "Downloading vanilla kernel ${KERNEL_MAJOR}${KERNEL_MINOR} ..."
+wget -nc "https://www.kernel.org/pub/linux/kernel/v4.x/linux-${KERNEL_MAJOR}${KERNEL_MINOR}.tar.xz" || exit 1
+wget -nc "https://www.kernel.org/pub/linux/kernel/v4.x/linux-${KERNEL_MAJOR}${KERNEL_MINOR}.tar.sign" || exit 1
 
 if [ -n "${BFS}" ]; then
 	big_echo "Downloading BFS patches ..."
-	wget -nc "http://ck.kolivas.org/patches/4.0/${KERNEL_MAJOR}/${KERNEL_MAJOR}-${BFS}/patch-${KERNEL_MAJOR}-${BFS}.bz2" || exit 1
-        wget -nc "https://raw.githubusercontent.com/hhoffstaette/kernel-patches/master/4.1/bfs-009-add-preempt_offset-argument-to-should_resched%28%29.patch" -O \
-            bfs-009-add-preempt_offset-argument-to-should_resched.patch || exit 1
+	wget -nc "http://ck.kolivas.org/patches/4.0/${KERNEL_MAJOR}/${KERNEL_MAJOR}-${BFS}/patch-${KERNEL_MAJOR}-${BFS}.bz2" || 
+	wget -nc "http://ck.kolivas.org/patches/4.0/${KERNEL_MAJOR}/${KERNEL_MAJOR}-${BFS}/patch-${KERNEL_MAJOR}-${BFS}.xz" || exit 1
+        if [ "${KERNEL_MAJOR}" == "4.1" -a "${BFS}" == "ck2" ]; then
+            wget -nc "https://raw.githubusercontent.com/hhoffstaette/kernel-patches/master/4.1/bfs-009-add-preempt_offset-argument-to-should_resched%28%29.patch" -O \
+                bfs-009-add-preempt_offset-argument-to-should_resched.patch || exit 1
+        fi
 fi
 
 big_echo "Downloading BFQ patches ..."
@@ -36,38 +43,47 @@ rm robots.txt
 
 if [ -n "${PREEMPT_RT}" ]; then
 	big_echo "Downloading PREEMPT_RT patches ..."
-	wget -nc "https://www.kernel.org/pub/linux/kernel/projects/rt/${KERNEL_MAJOR}/patch-${KERNEL_MAJOR}.${KERNEL_MINOR}-${PREEMPT_RT}.patch.xz" || exit 1
+	wget -nc "https://www.kernel.org/pub/linux/kernel/projects/rt/${KERNEL_MAJOR}/patch-${KERNEL_MAJOR}${KERNEL_MINOR}-${PREEMPT_RT}.patch.xz" || exit 1
 fi
 
 big_echo "Uncompresing kernel ..."
-xz -dk "linux-${KERNEL_MAJOR}.${KERNEL_MINOR}.tar.xz"
-gpg --verify "linux-${KERNEL_MAJOR}.${KERNEL_MINOR}.tar.sign" "linux-${KERNEL_MAJOR}.${KERNEL_MINOR}.tar" || exit 1
-tar xf "linux-${KERNEL_MAJOR}.${KERNEL_MINOR}.tar"
-rm "linux-${KERNEL_MAJOR}.${KERNEL_MINOR}.tar"
+xz -dk "linux-${KERNEL_MAJOR}${KERNEL_MINOR}.tar.xz"
+gpg --verify "linux-${KERNEL_MAJOR}${KERNEL_MINOR}.tar.sign" "linux-${KERNEL_MAJOR}${KERNEL_MINOR}.tar" || exit 1
+tar xf "linux-${KERNEL_MAJOR}${KERNEL_MINOR}.tar"
+rm "linux-${KERNEL_MAJOR}${KERNEL_MINOR}.tar"
 
 big_echo "Uncompresing patches ..."
 if [ -n "${BFS}" ]; then
-#	xz -d patch-${KERNEL_MAJOR}-${BFS}.xz
+    if [ -r "patch-${KERNEL_MAJOR}-${BFS}.xz" ]; then
+	xz -d patch-${KERNEL_MAJOR}-${BFS}.xz
+    elif [ -r "patch-${KERNEL_MAJOR}-${BFS}.bz2" ]; then
 	bzip2 -d patch-${KERNEL_MAJOR}-${BFS}.bz2
+    else
+        exit 1
+    fi
+
 	mv patch-${KERNEL_MAJOR}-${BFS} patch-${KERNEL_MAJOR}-${BFS}.patch
 fi
 if [ -n "${PREEMPT_RT}" ]; then
-	xz -d patch-${KERNEL_MAJOR}.${KERNEL_MINOR}-${PREEMPT_RT}.patch.xz
+	xz -d patch-${KERNEL_MAJOR}${KERNEL_MINOR}-${PREEMPT_RT}.patch.xz
 fi
 
-PATCHES="../000*.patch ../bfs-009-add-preempt_offset-argument-to-should_resched.patch"
+PATCHES="../000*.patch"
+if [ -r "../bfs-009-add-preempt_offset-argument-to-should_resched.patch" ]; then
+    PATCHES="${PATCHES} ../bfs-009-add-preempt_offset-argument-to-should_resched.patch"
+fi
 if [ -n "${BFS}" ]; then
 	PATCHES="../patch-${KERNEL_MAJOR}-${BFS}.patch ${PATCHES}"
 fi
 if [ -n "${PREEMPT_RT}" ]; then
-	PATCHES="../patch-${KERNEL_MAJOR}.${KERNEL_MINOR}-${PREEMPT_RT}.patch ${PATCHES}"
+	PATCHES="../patch-${KERNEL_MAJOR}${KERNEL_MINOR}-${PREEMPT_RT}.patch ${PATCHES}"
 fi
 if [ -n "${PREEMPT_RT}" -a -n "${BFS}" ]; then
 	big_echo "Applying patch for BFS patch ..."
 	patch -p0 < patch-${KERNEL_MAJOR}-${BFS}.patch.rt
 fi
 
-(cd linux-${KERNEL_MAJOR}.${KERNEL_MINOR}
+(cd linux-${KERNEL_MAJOR}${KERNEL_MINOR}
 for PATCH in ${PATCHES}; do
 	big_echo "Patching with ${PATCH} ..."
 	patch -p1 < ${PATCH}
@@ -76,8 +92,8 @@ done
 
 big_echo "Starting kernel configuration ..."
 
-cp ${CONFIG} linux-${KERNEL_MAJOR}.${KERNEL_MINOR}/.config || exit 1
-cd linux-${KERNEL_MAJOR}.${KERNEL_MINOR}
+cp ../${CONFIG} linux-${KERNEL_MAJOR}${KERNEL_MINOR}/.config || exit 1
+cd linux-${KERNEL_MAJOR}${KERNEL_MINOR}
 
 # ARM cross-compilation stuff
 #export $(dpkg-architecture -aarmhf); export CROSS_COMPILE=${CROSS_COMPILER_PREFIX=} export ARCH=arm
@@ -127,7 +143,7 @@ big_echo "Compiling kernel ..."
 sudo sh -c "CONCURRENCY_LEVEL=$((`grep -c '^processor' /proc/cpuinfo`+1)) \
 	fakeroot make-kpkg --initrd kernel_image kernel_headers modules_image; \
 	cd ..; \
-	rm -r linux-${KERNEL_MAJOR}.${KERNEL_MINOR}"
+	rm -r linux-${KERNEL_MAJOR}${KERNEL_MINOR}"
 
 # ARM cross-compilation stuff
 #	fakeroot make-kpkg --arch arm --cross-compile ${CROSS_COMPILER_PREFIX} --initrd \
